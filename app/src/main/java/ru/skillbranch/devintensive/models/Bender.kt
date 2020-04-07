d1ffe1cb1cd8985d67c15df65ea8600cc0beba32
Anna1970 <anna-1804@mail.ru>
2020-04-07 19:40:57 +0300

    hometask_3 without * 7

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -47,19 +47,24 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         NAME(question = "Как меня зовут?", answers = listOf("бендер","bender")){
             override fun nextQuestion(): Question = PROFESSION
         },
+
         PROFESSION(question = "Назови мою профессию?", answers = listOf("сгибальщик","bender")){
             override fun nextQuestion(): Question = MATERIAL
         },
+
         MATERIAL(question = "Из чего я сделан?",
                     answers = listOf("металл","дерево","metal","iron","wood")){
             override fun nextQuestion(): Question = BDAY
         },
+
         BDAY(question = "Когда меня создали?", answers = listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
         },
+
         SERIAL(question = "Мой серийный номер?", answers = listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
         },
+
         IDLE(question = "На этом все, вопросов больше нет", answers = listOf()){
             override fun nextQuestion(): Question = IDLE
         };
